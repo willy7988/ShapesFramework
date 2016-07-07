eb46b8aee1f672b55c4ab608b8ad47d79522a5c5
willy7988 <jonathan155013@gmail.com>
2016-07-07 11:55:03 +0800

    First J

---
+++ ShapesFramework/Shape.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesFramework
+{
+    abstract class Shape
+    {
+        public abstract string Discription();
+        public string Discription2()
+        {
+            return "This is a shape yo.";
+        }
+    }
+
+    class Circle : Shape
+    {
+        private double radius;
+        private double x;
+        private double y;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+            x = 0.0;
+            y = 0.0;
+        }
+
+        public Circle(double radius, double x, double y) 
+        {
+            this.radius = radius;
+            this.x = x;
+            this.y = y;
+        }
+
+        override public string Discription()
+        {
+            return string.Format("Circle: \n Origin = ({0},{1}), Radius = {2}", x, y, radius);
+        }
+    }
+
+
+    class Triangle : Shape
+    {
+        override public string Discription()
+        {
+            return "Triangle";
+        }
+    }
+}
---
+++ ShapesFramework/ShapeManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesFramework
+{
+    class ShapeManager
+    {
+        private int counter;
+
+        private static ShapeManager _instance;
+        public static ShapeManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new ShapeManager();
+                return _instance;
+            }
+        }
+
+        private ShapeManager()
+        {
+            counter = 0;
+        }
+
+        public void DoWork()
+        {
+            System.Console.WriteLine("You've done it " + (++counter) + " times.");
+        }
+    }
+}
