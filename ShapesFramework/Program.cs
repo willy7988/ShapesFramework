ef758663a8445049930682aa44de2bd25d9b4774
willy7988 <jonathan155013@gmail.com>
2016-07-07 12:03:26 +0800

    -hello

--- ShapesFramework/Program.cs
+++ ShapesFramework/Program.cs
@@ -10,10 +10,39 @@ namespace ShapesFramework
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Jake");
+            List<Shape> shapes = new List<Shape>();
 
-            System.Console.WriteLine("Hello World!");
+            shapes.Add(new Circle(5.5));
+            shapes.Add(new Circle(5.5, 1, 2));
+            shapes.Add(new Triangle());
 
+            foreach (Shape s in shapes)
+            {
+                System.Console.WriteLine(s.Discription());
+                System.Console.WriteLine(s.Discription2());
+                ShapeManager.Instance.DoWork();
+            }
+            Yo();
+            Program2.Yo();
             System.Console.ReadLine();
         }
+
+        static void Yo()
+        {
+            for (int i = 0; i < 5; ++i)
+                ShapeManager.Instance.DoWork();
+        }
+    }
+
+    class Program2
+    {
+
+        public static void Yo()
+        {
+            for (int i = 0; i < 5; ++i)
+                ShapeManager.Instance.DoWork();
+        }
+
     }
 }
